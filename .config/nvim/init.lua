d30435e4b008a94dace8a0f75bb22e93cde2aa16
Gabriel R. <gabrielmio16@gmail.com>
2024-11-17 11:45:59 -0300

    fix: hardcoded colorscheme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -14,4 +14,12 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup({
+  spec = {
+    { import = "plugins" }
+  },
+  change_detection = { notify = false }
+})
+
+vim.o.background = "dark"
+vim.cmd([[colorscheme gruvbox]])
