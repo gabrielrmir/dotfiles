6b773cc5060d1ad0c3b400cc35fc9fa41f300a14
Gabriel R. <gabrielmio16@gmail.com>
2024-11-17 11:44:48 -0300

    style: formatting

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,26 +1,27 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.6",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		require("telescope").setup({
-			defaults = {
-				mappings = {
-					i = {
-						["<C-h>"] = "which_key",
-					},
-				},
-			},
-			pickers = {},
-			extensions = {},
-		})
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.6",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    require("telescope").setup({
+      defaults = {
+        mappings = {
+          i = {
+            ["<C-h>"] = "which_key",
+          },
+        },
+      },
+      pickers = {},
+      extensions = {},
+    })
 
-		local builtin = require("telescope.builtin")
-		local notify_ext = require("telescope").extensions.notify.notify
-		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
-		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Grep Files" })
-		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find Buffers" })
-		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help Pages" })
-		vim.keymap.set("n", "<leader>fn", notify_ext, { desc = "Notification History" })
-	end,
+
+    local builtin = require("telescope.builtin")
+    local notify_ext = require("telescope").extensions.notify.notify
+    vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
+    vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Grep Files" })
+    vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find Buffers" })
+    vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help Pages" })
+    vim.keymap.set("n", "<leader>fn", notify_ext, { desc = "Notification History" })
+  end,
 }
