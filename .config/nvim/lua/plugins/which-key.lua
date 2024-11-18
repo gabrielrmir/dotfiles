4d210ff0f879ea499878976c436fc119a13158ad
Gabriel R. <gabrielmio16@gmail.com>
2024-11-18 13:43:59 -0300

    feat: change separator icon

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -1,17 +1,23 @@
 return {
-	"folke/which-key.nvim",
-	event = "VeryLazy",
-	init = function()
-		vim.o.timeout = true
-		vim.o.timeoutlen = 300
-	end,
-	opts = {},
-	config = function()
-		local wk = require("which-key")
-		wk.register({
-			f = { name = "+Find" },
-			b = { name = "+Buffer" },
-			x = { name = "+Trouble" },
-		}, { prefix = "<leader>" })
-	end,
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  init = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+  end,
+  config = function()
+    require("which-key").setup({
+      icons = {
+        breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
+        separator = ">", -- symbol used between a key and it's label
+        group = "+",    -- symbol prepended to a group
+      },
+    })
+    local wk = require("which-key")
+    wk.register({
+      f = { name = "+Find" },
+      b = { name = "+Buffer" },
+      x = { name = "+Trouble" },
+    }, { prefix = "<leader>" })
+  end,
 }
